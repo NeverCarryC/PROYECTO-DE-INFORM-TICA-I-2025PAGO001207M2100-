b29cf16c841b39b6a9cb29525c980e3f0f923e67
NeverCarryC <52173684+NeverCarryC@users.noreply.github.com>
2025-12-05 17:17:44 +0100

    2

--- src/controller/AlumnoMainController.java
+++ src/controller/AlumnoMainController.java
@@ -46,7 +46,7 @@ public class AlumnoMainController {
 							Parent view = loader.load();
 							 rootPane.setCenter(view);
 						} catch (IOException e) {
-							// TODO Auto-generated catch block
+						
 							e.printStackTrace();
 						}
 
